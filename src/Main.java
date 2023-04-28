86e2587f2b542b1ff3b3521d3fe566e5066c7da2
gotokaka <kakaporider510@gmail.com>
2023-04-28 20:54:09 +0900

    再トライ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("こんにちわ,後藤!");
+        System.out.println("こんにちわあ,後藤!");
     }
 }
